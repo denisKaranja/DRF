c6d255b4849a78010a2bd2a620da5c50e23b8284
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-05-22 12:47:01 +0300

    Urls bug fix
    Fix on Snippets urls

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -4,5 +4,5 @@ from django.contrib import admin
 urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include(snippets.urls)),
+    url(r'^', include('snippets.urls')),
 ]
