203f3e50364cadb50d9abe2ecbfff2c33229c638
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-05-22 23:25:54 +0300

    Hyperlink API
    Use hyperlinked style between entities

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -6,16 +6,17 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
   owner = serializers.ReadOnlyField(source = 'owner.username')
+  highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
   class Meta:
     model = Snippet
-    fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
+    fields = ('highlight', 'title', 'code', 'linenos', 'language', 'style', 'owner', 'url')
 
-class UserSerializer(serializers.ModelSerializer):
-  snippets = serializers.PrimaryKeyRelatedField(many = True, queryset = Snippet.objects.all())
+class UserSerializer(serializers.HyperLinkedModelSerializer):
+  snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
   class Meta:
     model = User
-    fields = ('id', 'username', 'snippets')
+    fields = ('url', 'username', 'snippets')
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from snippets import views
+
 urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
