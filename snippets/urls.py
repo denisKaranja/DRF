e1673247358664fef932bc2c2fc80ce5643eb0eb
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-05-22 23:04:59 +0300

    Create endpoints for highlighted snippets

--- snippets/urls.py
+++ snippets/urls.py
@@ -3,8 +3,10 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 unformated_urlpatterns = [
+  # snippets urls
   url(r'^snippets/$', views.SnippetList.as_view()),
   url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
+  url(r'^snippets/(?P<pk>[0-9]+)/highlight/$', views.SnippetHighlight.as_view()),
 
   # users urls
   url(r'^users/$', views.UserList.as_view()),
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.api_root),
     url(r'^', include('snippets.urls', namespace = 'snippets')),
     url(r'^api-login/', include('rest_framework.urls', namespace = 'rest_framework')),
 
