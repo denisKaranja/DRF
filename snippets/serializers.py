0476efbc74a0b9fd87bae3c6b6eb039a5a7109b3
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-05-23 21:17:51 +0300

    Fix snippet-detail url bug

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -7,14 +7,15 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
 class SnippetSerializer(serializers.HyperlinkedModelSerializer):
-  owner = serializers.ReadOnlyField(source = 'owner.username')
+  owner = serializers.ReadOnlyField(source='owner.username')
   highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
   class Meta:
     model = Snippet
-    fields = ('highlight', 'title', 'code', 'linenos', 'language', 'style', 'owner', 'url')
+    fields = ('url', 'highlight', 'owner','title', 'code', 'linenos', 'language', 'style')
 
-class UserSerializer(serializers.HyperLinkedModelSerializer):
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
   snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
   class Meta:
