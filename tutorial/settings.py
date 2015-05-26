c60a8d34c0eccfe273c97c0248c7ae02fa7fe9b9
Denis Karanja <deebeat@students.uonbi.ac.ke>
2015-05-26 22:38:07 +0300

    Update settings.py
    Modify Database settings

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'drf_dive_in',
         'USER': 'deebeat',
-        'PASSWORD': 'nannymcphee()[]',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
